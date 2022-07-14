b0adf28c72a227ca9821fabf536d5f0daa2cd8b0
Evan Igwilo <uvclassic@gmail.com>
2022-07-14 12:09:26 +0000

    REFACTOR: Refactor type definitions by grouping imports

---
+++ backend/src/api/v1/graphql/typeDefs/index.ts
@@ -0,0 +1,8 @@
+// 👇 Constants, Helpers & Types
+import userType from './userType';
+import postType from './postType';
+import messageType from './messageType';
+import mediaType from './mediaType';
+import notificationType from './notificationType';
+
+export default [userType, postType, messageType, mediaType, notificationType];
