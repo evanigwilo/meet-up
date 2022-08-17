28cf951dc4830d27deba82831d453c76fb0eb03e
Evan Igwilo <uvclassic@gmail.com>
2022-08-17 10:05:48 +0000

    STYLE: Added styled DashedBorder component

---
+++ frontend/src/styles/DashedBorder.tsx
@@ -0,0 +1,8 @@
+// 👇 Styled Component
+import styled from "styled-components";
+
+export default styled.div`
+  width: 100%;
+  border-bottom: 0.25em solid rgb(255 2455 255 / 10%);
+  border-bottom-style: groove;
+`;
