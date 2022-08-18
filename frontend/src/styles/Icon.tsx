c79dda2b2f23e5e8414e29289a72620e4a5ffc89
Evan Igwilo <uvclassic@gmail.com>
2022-08-18 16:38:25 +0000

    STYLE: Added styled Icon component

---
+++ frontend/src/styles/Icon.tsx
@@ -0,0 +1,15 @@
+// 👇 Styled Component
+import { css } from "styled-components";
+// 👇 Constants, Helpers & Types
+import { Spacing } from "../utils/types";
+
+export const IconCSS = css<Spacing>`
+  fill: white;
+  opacity: ${({ theme }) => theme.opacity.dim};
+  --dimension: ${({ theme }) => theme.sizing.icon};
+  width: var(--dimension);
+  height: var(--dimension);
+  overflow: unset;
+  margin: ${({ margin }) => margin};
+  padding: ${({ padding }) => padding};
+`;
