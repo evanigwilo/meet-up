454b48d894033cce35951fbb5df08b51cfa69bdc
Evan Igwilo <uvclassic@gmail.com>
2022-08-18 14:25:47 +0000

    STYLE: Added styled Font component

---
+++ frontend/src/styles/Font.tsx
@@ -0,0 +1,7 @@
+// 👇 Styled Component
+import { css } from "styled-components";
+
+export const CalligraffittiCSS = css`
+  /* font-family: "Calligraffitti", cursive; */
+  font-family: "Style Script", cursive;
+`;
